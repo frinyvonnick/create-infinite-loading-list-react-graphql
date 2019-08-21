85d4bd32e89bc658dfa3c6541c77f7dafae35788
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-08-21 14:16:02 +0200

    :tada: Set up front to use GraphQL server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import InfiniteList from './InfiniteList'
+
 import logo from './logo.svg';
 import './App.css';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <InfiniteList />
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/InfiniteList.hooks.js
@@ -0,0 +1,70 @@
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const GET_PERSONS = gql`
+query getPersons($cursor: ID) {
+  persons(first: 10, cursor: $cursor) {
+    edges {
+      node {
+        lastname
+        firstname
+      }
+    }
+    pageInfo {
+      endCursor
+      hasNextPage
+    }
+  }
+}
+`
+
+function usePersons() {
+  const {
+    data,
+    loading,
+    fetchMore,
+  } = useQuery(GET_PERSONS, {
+    notifyOnNetworkStatusChange: true,
+  })
+
+  console.log('useQuery', {
+    data,
+    loading,
+  })
+
+  if (loading && !data.persons) return { loading, persons: [] }
+
+  const loadMore = () => {
+    return fetchMore({
+      query: GET_PERSONS,
+      notifyOnNetworkStatusChange: true,
+      variables: {
+        cursor: data.persons.pageInfo.endCursor,
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const newEdges = fetchMoreResult.persons.edges
+        const pageInfo = fetchMoreResult.persons.pageInfo
+
+        return newEdges.length
+          ? {
+            persons: {
+              // eslint-disable-next-line no-underscore-dangle
+              __typename: previousResult.persons.__typename,
+              edges: [...previousResult.persons.edges, ...newEdges],
+              pageInfo,
+            },
+          }
+          : previousResult
+      },
+    })
+  }
+
+  return {
+    persons: data.persons.edges.map(({ node }) => node),
+    hasNextPage: data.persons.pageInfo.hasNextPage,
+    loading,
+    loadMore,
+  }
+}
+
+export default usePersons;
