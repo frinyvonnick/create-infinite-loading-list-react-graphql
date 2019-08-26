a0e8994c6ca0b9e5933b4dca26ead8b61013444e
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-08-26 11:01:31 +0200

    :recycle: Refactor infinite loading list using react-simple-infite-loading

--- src/InfiniteList.js
+++ src/InfiniteList.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { FixedSizeList as List } from 'react-window'
-import InfiniteLoader from 'react-window-infinite-loader'
-import AutoSizer from 'react-virtualized-auto-sizer'
+import InfiniteLoadingList from 'react-simple-infinite-loading'
 
 import usePersons from './InfiniteList.hooks'
 
@@ -10,44 +8,29 @@ import './InfiniteList.css'
 function InfiniteList() {
   const { persons, loading, loadMore, hasNextPage } = usePersons()
 
-  const personsCount = hasNextPage ? persons.length + 1 : persons.length
   const loadMorePersons = loading ? () => {} : loadMore
   const isPersonLoaded = index => !hasNextPage || index < persons.length
 
   return (
     <div className="InfiniteList-list">
-      <AutoSizer>
-        {({ height, width }) => (
-          <InfiniteLoader
-            isItemLoaded={isPersonLoaded}
-            itemCount={personsCount}
-            loadMoreItems={loadMorePersons}
-          >
-            {({ onItemsRendered, ref }) => (
-              <List
-                height={height}
-                itemCount={personsCount}
-                itemSize={40}
-                onItemsRendered={onItemsRendered}
-                ref={ref}
-                width={width}
-              >
-                {({ index, style }) => {
-                  let content
-                  if (!isPersonLoaded(index)) {
-                    content = 'Loading...'
-                  } else {
-                    const { firstname, lastname } = persons[index]
-                    content = `${firstname} ${lastname}` 
-                  }
+      <InfiniteLoadingList
+        items={persons}
+        itemHeight={40}
+        hasMoreItems={hasNextPage}
+        loadMoreItems={loadMorePersons}
+      >
+        {({ item: person, index, style }) => {
+          let content
+          if (!isPersonLoaded(index)) {
+            content = 'Loading...'
+          } else {
+            const { firstname, lastname } = person
+            content = `${firstname} ${lastname}` 
+          }
 
-                  return <div className="InfiniteList-item" style={style}>{content}</div>
-                }}
-              </List>
-            )}
-          </InfiniteLoader>
-        )}
-      </AutoSizer>
+          return <div className="InfiniteList-item" style={style}>{content}</div>
+        }}
+      </InfiniteLoadingList>
     </div>
   )
 }
